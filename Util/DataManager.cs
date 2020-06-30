c97b58506f5e883ff68192d071e5a8978e2c420a
Keita <koron0902@gmail.com>
2020-06-30 10:39:28 +0900

    Parcelable.Dataを分離

--- Util/DataManager.cs
+++ Util/DataManager.cs
@@ -1,111 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
-using Android.OS;
-using Android.Runtime;
-using Java.Interop;
 using System.Linq;
+using Android.Database;
+using Android.Database.Sqlite;
+using Android.Content;
 
 namespace GoodByeMilk.Util {
-  public class Data : Java.Lang.Object, IParcelable {
-    public enum Kind {
-      MONING = 0,
-      NOON,
-      EVENING,
-      SNACK
-    };
+  public class DataManager : SQLiteOpenHelper {
+    List<BabyFood> data_;
+    public IReadOnlyList<BabyFood> dataRef_;
+    static string databaseName_ => "babyFod.db";
+    static int databaseVersion_ = 1;
 
-    public static string ToString(Kind _kind) {
-      switch(_kind) {
-      case Kind.MONING:
-        return "朝";
-      case Kind.NOON:
-        return "昼";
-      case Kind.EVENING:
-        return "夜";
-      case Kind.SNACK:
-        return "おやつ";
-      default:
-        return "";
-      }
-    }
 
-    public Kind kind_;
-    public DateTime date_;
-    public string menu_;
-    public string unit_;
-    public int quantity_;
-    public string hash_;
-    public Data(Kind _kind, DateTime _date, string _menu, string _unit, int _quant) {
-      kind_ = _kind;
-      date_ = _date;
-      menu_ = _menu;
-      unit_ = _unit;
-      quantity_ = _quant;
-      var hashByte = System.Security.Cryptography.SHA512.Create().ComputeHash(((new System.Text.ASCIIEncoding()).GetBytes(kind_ + date_.ToString("yyyyMMdd") + menu_ + unit_ + quantity_)));
-
-      var builder = new System.Text.StringBuilder();
-      foreach(var b in hashByte) {
-        builder.Append(b.ToString("x2"));
-      }
-      hash_ = builder.ToString();
-    }
-
-    public int DescribeContents() {
-      return 0;
-    }
-
-    public void WriteToParcel(Parcel dest, [GeneratedEnum] ParcelableWriteFlags flags) {
-      dest.WriteInt((int)kind_);
-      dest.WriteString(date_.ToString("yyyy-MM-dd"));
-      dest.WriteString(menu_);
-      dest.WriteString(unit_);
-      dest.WriteInt(quantity_);
-    }
-
-    [ExportField("CREATOR")]
-    public static IParcelableCreator GetCreator() {
-      return new MyParcelableCreator();
-    }
-
-    // Parcelable.Creator の代わり
-    class MyParcelableCreator : Java.Lang.Object, IParcelableCreator {
-      #region IParcelableCreator implementation
-      Java.Lang.Object IParcelableCreator.CreateFromParcel(Parcel source) {
-        var kind = (Kind)source.ReadInt();
-        var date = DateTime.Parse(source.ReadString());
-        var menu = source.ReadString();
-        var unit = source.ReadString();
-        var quantity = source.ReadInt();
-
-        return new Data(kind, date, menu, unit, quantity);
-      }
-
-      Java.Lang.Object[] IParcelableCreator.NewArray(int size) {
-        return new Java.Lang.Object[size];
-      }
-      #endregion
-    }
-  }
-
-  public class DataManager {
-    List<Data> data_;
-    public IReadOnlyList<Data> dataRef_;
-
-    public DataManager() {
-      data_ = new List<Data>();
+    public DataManager(Context context) : base(context, System.IO.Path.Combine(context.GetExternalFilesDir(null).Path, databaseName_), null, databaseVersion_) {
+      data_ = new List<BabyFood>();
       dataRef_ = data_;
-      data_.Add(new Data(Data.Kind.EVENING, DateTime.Today, "aa", "aa", 1));
-      data_.Add(new Data(Data.Kind.EVENING, DateTime.Today, "aa", "aa", 1));
-      data_.Add(new Data(Data.Kind.EVENING, DateTime.Today, "aa", "aa", 1));
-      data_.Add(new Data(Data.Kind.EVENING, DateTime.Today, "aa", "aa", 1));
+      data_.Add(new BabyFood(BabyFood.Kind.EVENING, DateTime.Today, "aa", "aa", 1));
+      data_.Add(new BabyFood(BabyFood.Kind.EVENING, DateTime.Today, "aa", "aa", 1));
+      data_.Add(new BabyFood(BabyFood.Kind.EVENING, DateTime.Today, "aa", "aa", 1));
+      data_.Add(new BabyFood(BabyFood.Kind.EVENING, DateTime.Today, "aa", "aa", 1));
+      data_.AddRange()
     }
 
-    public void AddData(List<Data> _new) {
+
+    public void AddData(List<BabyFood> _new) {
       data_.AddRange(_new);
     }
 
-    public void RemoveData(List<Data> _remove) {
+    public void RemoveData(List<BabyFood> _remove) {
       data_.RemoveAll(elm => _remove.Where(mle => elm.hash_ == mle.hash_).Count() != 0);
+    }
+
+    public void RemoveData(BabyFood _remove) {
+      data_.RemoveAll(elm => elm.hash_ == _remove.hash_);
+    }
+
+    public override void OnCreate(SQLiteDatabase db) {
+      throw new NotImplementedException();
+    }
+
+    public override void OnUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+      throw new NotImplementedException();
+    }
+
+    void Remove() {
+
+    }
+
+    void Insert() {
+
+    }
+
+    void Update() {
+
+    }
+
+    IEnumerable<BabyFood> Select() {
+      return new List<BabyFood>();
     }
   }
 }
